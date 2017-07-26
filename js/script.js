29c2648439c000472b37a2c02409045f4b5d91bf
Kelvin Bunei <keighb@live.com>
2017-07-26 05:58:51 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+<script type="text/javascript" charset="utf-8">
+			$(window).load(function() {
+				$('.flexslider').flexslider({
+					directionNav: false,
+					slideshowSpeed: 10000,
+					animationDuration: 1000,
+				});
+			});
+		</script>
